bd4b4fc21af28a0fe62162fbc67b0f7ab4b3175c
Juan Manuel Velez Torres <juanmanuelveleztorres@gmail.com>
2021-12-26 12:47:04 +0100

    add middlewares

---
+++ src/server.go
@@ -0,0 +1,36 @@
+package main
+
+import "net/http"
+
+type Server struct {
+	port string
+	router *Router
+}
+
+func NewServer(port string) *Server{
+	return &Server{
+		port: port,
+		router: NewRouter(),
+	}
+}
+
+func (s *Server) Handler(path string, handler http.HandlerFunc){
+	s.router.rules[path] = handler
+}
+
+func (s *Server) AddMiddleware(f http.HandlerFunc,middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares{
+		f = m(f) //we send the middleware as parameter
+	}
+	return f
+}
+
+func (s *Server) Listen() error{
+	http.Handle("/", s.router) //entry point
+
+	err := http.ListenAndServe(s.port, nil)
+	if( err != nil){
+		return err
+	}
+	return nil
+}
